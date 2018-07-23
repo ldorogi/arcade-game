efab6a1df633e2ae4c500c2561754dcb3fb099c2
László Dorogi <laszlo.dorogi.68@gmail.com>
2018-07-23 16:43:08 +0200

    Player and enemies realisation with collision detection

--- js/app.js
+++ js/app.js
@@ -1,36 +1,107 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+class Enemy {
+    constructor(x, y) {
+        // The image/sprite for our enemies, this uses
+        // a helper we've provided to easily load images
+        this.sprite = 'images/enemy-bug.png';
+        this.speed = Math.round(Math.random() * 2) + 1;
+        setTimeout(()=> {
+            this.x = -100;
+            this.y = [60,145,230][Math.floor(Math.random() * 3)];
+        }, this.speed * 100);
+    }
+    
+    // Update the enemy's position, required method for game
+    // Parameter: dt, a time delta between ticks
+    update(dt) {
+        // You should multiply any movement by the dt parameter
+        // which will ensure the game runs at the same speed for
+        // all computers.
+        this.x = (this.x + dt * this.speed * 100) % (500);
+        // if enemy dissapears on the right edge than reset it
+        if(this.x >= 450) {
+            this.x = -100;
+            this.y = [60,145,230][Math.floor(Math.random() * 3)];
+            this.speed = Math.round(Math.random() * 2) + 1;
+        }
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
+        // collision detection between the player and the enemy
+        if(Math.abs(Math.floor(player.x) - Math.floor(this.x)) <= 60 &&
+            Math.abs(Math.floor(player.y) - Math.floor(this.y)) <= 13) {
+            window.location.reload();
+        }
+    }
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
+    // Draw the enemy on the screen, required method for game
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+}
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+
+
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+class Player{
+    constructor(x, y) {
+        this.sprite = 'images/char-pink-girl.png';
+        this.x = x;
+        this.y = y;
+        this.won = false;
+    }
 
+    update() {
+
+    }
+
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+
+    handleInput (move) {
+        let xDelta = 101;
+        let yDelta = 83;
+        if (move === 'left') {
+            this.x -= this.x >= xDelta ? xDelta : 0;
+        }
+        if (move === 'right'){
+            this.x += this.x < 4 * xDelta ? 101 : 0;
+        }
+        if (move === 'down') {
+            this.y += this.y < 5 * 80 ? yDelta : 0; 
+        }
+        if (move === 'up') {
+            if (this.y >= yDelta) {
+                this.y -= yDelta
+            } else {
+                // game won
+                //document.querySelector('.wrap-hide').classList.remove('wrap-hide');
+                this.reset();
+                //document.querySelector('canvas').style.display = 'none';
+            }
+        }
+    }
+
+    reset() {
+        this.x = 2 * 101;
+        this.y = 5 * 80;
+    }
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
+// If you wold like to make the game more difficult, than increase 
+// the difficultLevel (how many enemies are in the same time on the gameboard)
+const allEnemies = [];
+const difficultLevel = 2;
+for(let i = 0; i < difficultLevel; i++) {
+    allEnemies[i] = new Enemy(1, 1);
+}
+
 // Place the player object in a variable called player
-
-
+const player = new Player(2 * 101, 5 * 80);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
